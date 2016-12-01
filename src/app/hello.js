ad1df3115b09693c4f434e07870f5d37970c4017
Richard Harrah <richard.harrah2@anthem.com>
2016-12-01 09:52:42 -0500

    fixing Hello class

--- src/app/hello.js
+++ src/app/hello.js
@@ -4,13 +4,16 @@ import React, {Component} from 'react';
  * Hello class
  */
 export class Hello extends Component {
+
+  // This should not be static
   /**
    * Render method for Hello class
    * @return {XML}
    */
-  static render() {
+  render() {
     return (
       <h1>{'Hello world!'}</h1>
     );
   }
+
 }
