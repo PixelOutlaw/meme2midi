955b6446f89ea3ab4c7e50393ffd203141fed5eb
Richard Harrah <richard.harrah2@anthem.com>
2016-12-01 09:14:40 -0500

    updating eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'extends': [
+    'google',
+    'xo-react/space',
+    'xo-space/esnext'
+  ],
+  'env': {
+    'browser': true,
+    'jasmine': true
+  },
+  'plugins': [
+    'babel',
+    'jasmine',
+    'react'
+  ]
+};
--- src/app/hello.js
+++ src/app/hello.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 
+/**
+ * Hello class
+ */
 export class Hello extends Component {
-  render() {
+  /**
+   * Render method for Hello class
+   * @return {XML}
+   */
+  static render() {
     return (
       <h1>{'Hello world!'}</h1>
     );
